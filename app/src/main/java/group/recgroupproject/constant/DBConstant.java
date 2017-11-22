5048989be0918d7092e8102a8c5b9d1642a3d86c
tmaguiar <31488583+tmaguiar@users.noreply.github.com>
2017-11-22 12:18:41 -0500

    DB Setup

---
+++ app/src/main/java/group/recgroupproject/constant/DBConstant.java
@@ -0,0 +1,15 @@
+package group.recgroupproject.constant;
+
+/**
+ * Constants related to database file
+ * Such as file path and file name
+ */
+public abstract class DBConstant
+{
+    //database file directory
+    public static String DATABASE_PATH = "/data/data/group.recgroupproject/databases";
+    //database file name
+    public static String DATABASE_FILE = "events.db";
+    //database version
+    public static int DATABASE_VERSION = 1;
+}
---
+++ app/src/main/java/group/recgroupproject/constant/SQLCommand.java
@@ -0,0 +1,7 @@
+package group.recgroupproject.constant;
+
+public abstract class SQLCommand
+{
+    //query all students
+    public static String QUERY_STUDENT = "select stid, stname from Student";
+}
